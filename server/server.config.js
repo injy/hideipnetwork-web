f9cd473cedaf609018d9624f22ae4cece14da86b
injy <injy@qq.com>
2022-12-29 18:05:06 +0800

    Update server.config.js

--- server/server.config.js
+++ server/server.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
-    prefix: `/server/`,
-    bare: `https://cfxx.diy.gd`,
+    prefix: '/server/',
+    bare: 'https://cfxx.diy.gd',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/server/server.handler.js',
